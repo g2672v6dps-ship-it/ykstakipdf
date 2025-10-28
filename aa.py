6dcfcda5340d483d9e7c56e54c31c1685d7a55c0
g2672v6dps-ship-it <g2672v6dps@privaterelay.appleid.com>
2025-10-28 21:34:11 +0300

    Update aa.py

--- aa.py
+++ aa.py
@@ -14318,7 +14318,7 @@ def main():
     ">
         <img src="https://raw.githubusercontent.com/g2672v6dps-ship-it/ykstakipdf/0538075040afbf31b8a262ce85d449137fbdac30/logodf.png"
              alt="Psiko DSF Logo"
-             style="width:100px; height:100px; border-radius:15px;">
+             style="width:160px; height:160px; border-radius:20px;">
     </div>
     """,
     unsafe_allow_html=True
