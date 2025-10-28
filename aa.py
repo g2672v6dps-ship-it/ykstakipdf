56d7bc1c8dcf0f8223bbcad280116d554581b805
g2672v6dps-ship-it <g2672v6dps@privaterelay.appleid.com>
2025-10-28 22:01:56 +0300

    Update aa.py

--- aa.py
+++ aa.py
@@ -14314,7 +14314,7 @@ def main():
         align-items: center;
         justify-content: center;
        
-    ">
+    
         <img src="https://raw.githubusercontent.com/g2672v6dps-ship-it/ykstakipdf/0538075040afbf31b8a262ce85d449137fbdac30/logodf.png"
              alt="Psiko DSF Logo"
              style="width:150px; height:150px; border-radius:20px;">
