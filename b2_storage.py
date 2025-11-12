f4aecbd12833f2405fdfc41e20c3dc716831606b
g2672v6dps-ship-it <g2672v6dps@privaterelay.appleid.com>
2025-11-13 01:22:28 +0300

    Add files via upload

---
+++ b2_storage.py
@@ -0,0 +1,26 @@
+import os
+import boto3
+from botocore.client import Config
+
+B2_ENDPOINT = os.getenv("Master Application Key")
+B2_KEY = os.getenv("f69accbc6328")
+B2_SECRET = os.getenv("0036683481420f7d06274bd7b343e5cc6e53e5257a")
+B2_BUCKET = os.getenv("psikodonus-files")
+
+s3 = boto3.client(
+    service_name='s3',
+    endpoint_url=B2_ENDPOINT,
+    aws_access_key_id=B2_KEY,
+    aws_secret_access_key=B2_SECRET,
+    config=Config(signature_version='s3v4')
+)
+
+def upload_to_b2(file_path, object_name):
+    s3.upload_file(file_path, B2_BUCKET, object_name)
+    return f"{B2_ENDPOINT}/{B2_BUCKET}/{object_name}"
+
+def download_from_b2(object_name, destination_path):
+    s3.download_file(B2_BUCKET, object_name, destination_path)
+
+def delete_from_b2(object_name):
+    s3.delete_object(Bucket=B2_BUCKET, Key=object_name)
