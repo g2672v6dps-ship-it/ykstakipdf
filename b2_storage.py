abbb6a3367aaefb410790aed04cc809b8f3b5589
g2672v6dps-ship-it <g2672v6dps@privaterelay.appleid.com>
2025-11-13 11:38:25 +0300

    Update b2_storage.py

--- b2_storage.py
+++ b2_storage.py
@@ -8,7 +8,7 @@ B2_ENDPOINT = "s3.eu-central-003.backblazeb2.com"
 # API Anahtarları (Streamlit Cloud Secrets ile aynı)
 BACKBLAZE_APPLICATION_KEY_ID = "f69accbc6328"  # B2_KEY_ID
 BACKBLAZE_APPLICATION_KEY = "5f7639dabc9c7b2c96a30218"  # B2_APP_KEY
-BACKBLAZE_BUCKET_NAME = "psikodf-files"  # B2_BUCKET_NAME
+BACKBLAZE_BUCKET_NAME = "psikodonus-files"  # B2_BUCKET_NAME
 
 # İzin verilen yetkiler (opsiyonel)
 BUCKET_PERMISSIONS = "allPrivate"
