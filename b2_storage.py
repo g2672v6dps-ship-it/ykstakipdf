05216165eebc23033578517720ed2350bfd0ce16
g2672v6dps-ship-it <g2672v6dps@privaterelay.appleid.com>
2025-11-13 13:19:01 +0300

    Refactor Backblaze B2 configuration variables

--- b2_storage.py
+++ b2_storage.py
@@ -3,11 +3,12 @@
 
 # Gerçek API anahtarlarınızı buraya yazın:
 # B2 Endpoint
-BACKBLAZE_APPLICATION_KEY_ID = "003f69accbc63280000000001"
-BACKBLAZE_APPLICATION_KEY = "K003OMsFWIvniVkyYIhP1yjuQnElwZ4"
-BACKBLAZE_BUCKET_NAME = "psikodonustr-files"
-BACKBLAZE_ENDPOINT = "https://s3.eu-central-003.backblazeb2.com"
+B2_ENDPOINT = "s3.eu-central-003.backblazeb2.com"
 
+# API Anahtarları (Streamlit Cloud Secrets ile aynı)
+BACKBLAZE_APPLICATION_KEY_ID = "003f69accbc63280000000001"  # B2_KEY_ID
+BACKBLAZE_APPLICATION_KEY = "K003OMsFWIvniVkyYIhP1yjuQnElwZ4"  # B2_APP_KEY
+BACKBLAZE_BUCKET_NAME = "psikodonustr-files"  # B2_BUCKET_NAME
 
 # İzin verilen yetkiler (opsiyonel)
 BUCKET_PERMISSIONS = "allPrivate"
